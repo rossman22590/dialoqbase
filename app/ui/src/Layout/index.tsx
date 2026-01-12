05bc7751a361d9b0b770fa486b756968cf1ea7d0
Ross Cohen <rosscohen1990@gmail.com>
2026-01-12 14:49:38 -0500

    buidl

--- app/ui/src/Layout/index.tsx
+++ app/ui/src/Layout/index.tsx
@@ -44,74 +44,73 @@ export default function DashboardLayout({
         >
           {() => (
             <>
-              <div className="flex items-center">
-                <Link
-                  to="/"
-                  className="focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-700 flex items-center mr-4"
-                >
-                  <img
-                    className="h-8 w-auto"
-                    src="/logo.png"
-                    alt="Dialoqbase"
-                  />
-                  <span className="ml-1 text-xl font-bold dark:text-white">
-                    Botcraft Pro
-                  </span>
-                  <span className="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800 ml-2">
-                    {/* @ts-ignore */}
-                    {`v${__APP_VERSION__}`}
-                  </span>
-                </Link>
-              </div>
-              <div className=" ml-6 flex items-center">
-                <div className="mr-4 text-sm font-medium text-gray-700 dark:text-gray-200 border px-3 py-1 rounded-full border-gray-300 dark:border-gray-600">
-                  Credits: ${Number(creditData?.balance || 0).toFixed(4)}
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-14 justify-between">
+                  <Link
+                    to="/"
+                    className="focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-700 flex items-center mr-4"
+                  >
+                    <img
+                      className="h-8 w-auto"
+                      src="/logo.png"
+                      alt="Dialoqbase"
+                    />
+                    <span className="ml-1 text-xl font-bold dark:text-white">
+                      Botcraft Pro
+                    </span>
+                    <span className="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800 ml-2">
+                      {/* @ts-ignore */}
+                      {`v${__APP_VERSION__}`}
+                    </span>
+                  </Link>
+                  <div className=" ml-6 flex items-center">
+                    <div className="mr-4 text-sm font-medium text-gray-700 dark:text-gray-200 border px-3 py-1 rounded-full border-gray-300 dark:border-gray-600">
+                      Credits: ${Number(creditData?.balance || 0).toFixed(4)}
+                    </div>
+                    <ApplicationMenu />
+                  </div>
                 </div>
-                <ApplicationMenu />
               </div>
-            </div>
-      </div>
 
-      <Disclosure.Panel className="sm:hidden">
-        <div className="border-t border-gray-200 pt-4 pb-3">
-          <div className="flex items-center px-4">
-            <div className="flex-shrink-0">
-              <Avatar username={profile?.username || "admin"} />
-            </div>
-            <div className="ml-3">
-              <div className="text-base font-medium text-gray-800">
-                {profile?.username}
-              </div>
-            </div>
-          </div>
-          <div className="mt-3 space-y-1">
-            <Disclosure.Button
-              as={Link}
-              to="/settings"
-              className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-            >
-              Settings
-            </Disclosure.Button>
-            <Disclosure.Button
-              onClick={() => {
-                logout();
-                navigate("/login");
-              }}
-              className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-            >
-              Sign out
-            </Disclosure.Button>
-          </div>
+              <Disclosure.Panel className="sm:hidden">
+                <div className="border-t border-gray-200 pt-4 pb-3">
+                  <div className="flex items-center px-4">
+                    <div className="flex-shrink-0">
+                      <Avatar username={profile?.username || "admin"} />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium text-gray-800">
+                        {profile?.username}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-3 space-y-1">
+                    <Disclosure.Button
+                      as={Link}
+                      to="/settings"
+                      className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Settings
+                    </Disclosure.Button>
+                    <Disclosure.Button
+                      onClick={() => {
+                        logout();
+                        navigate("/login");
+                      }}
+                      className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Sign out
+                    </Disclosure.Button>
+                  </div>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <div>
+          <main>{children}</main>
         </div>
-      </Disclosure.Panel>
-    </>
-  )
-}
-        </Disclosure >
-  <div>
-    <main>{children}</main>
-  </div>
-      </div >
+      </div>
     </>
   );
 }
